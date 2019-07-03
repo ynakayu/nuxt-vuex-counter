b4d72bb8edb6c5ac5611257b62a7b3876aff637e
ynakayu <9111754+ynakayu@users.noreply.github.com>
2019-07-03 15:45:32 +0900

    fixed typo

--- components/Counter.vue
+++ components/Counter.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <h2>{{ title }}</h2>
-    counter: {{ $store.state.couter.couter }}
+    counter: {{ $store.state.counter.counter }}
     <button v-on:click="$store.commit('counter/countUp')">+1</button>
   </section>
 </template>
